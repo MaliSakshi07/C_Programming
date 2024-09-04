584c0dea8e2c75ff4bf980f472550df3d0bcd67a
MaliSakshi07 <sakshimali467@gmail.com>
2024-09-04 21:58:28 +0530

    Looping Statement

---
+++ Control & Looping Statements/09 For Loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0;
+
+    for(i = 0; i < 10; i++)
+    {
+        printf("\nWelcome");
+    }
+    printf("\n\nThanks!!!");
+
+    getch();
+    return 0;
+}
