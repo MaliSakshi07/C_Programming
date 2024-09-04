6074800f7df84da898257585f823ca8abeb6bb27
MaliSakshi07 <sakshimali467@gmail.com>
2024-09-04 21:57:13 +0530

    Looping Statement

---
+++ Control & Looping Statements/08 Do-While Loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0;
+
+    do
+    {
+       printf("\nWelcome",i);
+       i++;
+    }while(i < 10);
+
+    printf("\n\nThanks!!!");
+
+    getch();
+    return 0;
+}
+
