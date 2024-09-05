0730c904c1e6e1f7713cbbde1ebe921d820103dd
MaliSakshi07 <sakshimali467@gmail.com>
2024-09-05 21:08:13 +0530

    Looping Statement

---
+++ Control & Looping Statements/10 Program to Print Table of 17.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0;
+
+    printf("\nTable of 17 =>\n");
+    for(i =1; i <=10; i++)
+    {
+        printf("\t%d\n",i*17);
+    }
+
+    printf("\nThanks!!!");
+
+    getch();
+    return 0;
+}
