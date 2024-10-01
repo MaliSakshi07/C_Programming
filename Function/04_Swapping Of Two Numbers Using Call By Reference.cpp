ef93f17df94c2c9180ffff0c11844032cbd2bc32
MaliSakshi07 <sakshimali467@gmail.com>
2024-10-01 21:43:44 +0530

    Function

---
+++ Function/04_Swapping Of Two Numbers Using Call By Reference.cpp
@@ -0,0 +1,35 @@
+using namespace std;
+
+#include<stdio.h>
+#include<conio.h>
+#include<iostream>
+void swap(int& ,int&);
+
+int main()
+{
+    int No1 = 0, No2 = 0;
+
+    printf("\n Enter Two Numbers:");
+    scanf("%d%d",&No1,&No2);
+
+    printf("Given Numbers Before Swapping =>");
+    printf("\n\t No1 = %d \t No2 = %d",No1,No2);
+
+    printf("\n==================================\n");
+
+    swap(No1,No2);
+
+    printf("Given Numbers After Swapping =>");
+    printf("\n\t No1 = %d \t No2 = %d",No1,No2);
+
+    getch();
+    return 0;
+}
+void swap(int &R1,int &R2)
+{
+    int Temp = 0;
+
+    Temp = R1;
+    R1 = R2;
+    R2 = Temp;
+}
