73f103f584e9f36343f6e824807721998996cfc6
MaliSakshi07 <sakshimali467@gmail.com>
2025-02-11 21:46:50 +0530

    Pattern

---
+++ Pattern Printing/26 Reverse Pyramid.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0, j=0, x=0;
+
+    printf("\nEnter a value for Pattern :");
+    scanf("%d",&x);
+
+    for(i=x; i>=1; i--)
+    {
+        for(j=1; j<=x-i; j++)
+        {
+            printf("   ");
+        }
+        for(j=1; j<=2*i-1; j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
