31bfd428b3be96cf530c260b7ee55d5c59b9834c
MaliSakshi07 <sakshimali467@gmail.com>
2025-02-13 06:36:58 +0530

    Bitwise

---
+++ Bitwise/03 Bitwise - XOR.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No1 = 0, No2 = 0, Res = 0;
+
+    printf("\nEnter 1st Number :");
+    scanf("%d",&No1);
+
+    printf("\nEnter 2nd Number :");
+    scanf("%d",&No2);
+
+    Res = No1 ^ No2;
+
+    printf("\nResult %d ^ %d => %d ",No1,No2,Res);
+
+    getch();
+    return 0;
+}
